9ed6135f80fdc3da281628ff5b5d14780d08024b
Lahari Tenneti <93247239+laharitenneti@users.noreply.github.com>
2025-09-06 14:17:01 +0530

    Update Scanner.java
    Complete Scanner implementation with multi-character lexeme handling.

--- lahari-jlox/src/jloxinterpreter/Scanner.java
+++ lahari-jlox/src/jloxinterpreter/Scanner.java
@@ -14,6 +14,28 @@ class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 0;
+    private static final Map<String, TokenType> keywords;
+    
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+    
     //constructor
     Scanner(String source) {
         this.source = source;
@@ -51,10 +73,95 @@ class Scanner {
                 addToken(match('=') ? LESS_EQUAL : LESS); break;
             case '>':
                 addToken(match('=') ? GREATER_EQUAL : GREATER); break;
-            default:
-                Lox.error(line, "Unexpected character!");
+            //for parsing (& skipping) comments
+            case '/':
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                }
+                //multineline comments
+                else if (match('*')) {
+                    while (!isAtEnd()) {
+                        char ch = advance();
+                        if (ch == '\n') line++;
+                        if (ch == '*' && peek() == '/') {
+                            advance();
+                            return;
+                        }
+                    }
+                    Lox.error(line, "Unterminated comment.");
+                }
+                else {
+                    addToken(SLASH);
+                }
                 break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"': string(); break;
+            default:
+                if (isDigit(c)) {
+                    number();
+                }
+                else if (isAlpha(c)) {
+                    identifier();
+                }
+                else {
+                    Lox.error(line, "Unexpected character!");
+                }
+            break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+        
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text); //keyword
+        if(type == null) type = IDENTIFIER; //variable
+        addToken(type);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+        //looking for decimal point
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+            while (isDigit(peek())) advance();
+        }
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private char peekNext() {
+        if (current+1 >= source.length()) return '\0';
+        return source.charAt(current+1);
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            //bcs lox allows multiline strings, and the line must be updated hence
+            if (peek() == '\n') line++;
+            advance();
+        }
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string!");
+            return;
+        }
+        advance();
+        //just store the string content in value
+        String value = source.substring(start+1, current-1);
+        addToken(STRING, value);
     }
 
     private boolean match(char expected) {
@@ -63,6 +170,15 @@ class Scanner {
 
         current++;
         return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 
     private boolean isAtEnd() {
