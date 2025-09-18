da0f19ec59b38876d552ecd7ef8fd64d62204c52
Lahari Tenneti <93247239+laharitenneti@users.noreply.github.com>
2025-09-18 17:01:52 +0530

    Update Lox.java
    Error handling and changing the run() function to parse expressions.

--- lahari-jlox/src/jloxinterpreter/Lox.java
+++ lahari-jlox/src/jloxinterpreter/Lox.java
@@ -33,14 +33,22 @@ public class Lox {
     }
 
 
-    //won't work because I don't have a Scanner class yet
     //prints tokens the scanner will emit
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        /*
         for (Token token: tokens) {
             System.out.println(token);
         }
+         */
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
+
     }
 
     //error handling
@@ -51,6 +59,14 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme +"'", message);
+        }
     }
 
     public static void main(String[] args) throws IOException {
