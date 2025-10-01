089ad4dc658ef699b10bb50ebbe50d1660dfb6b4
Lahari <laharitenneti@pm.me>
2025-10-01 16:01:01 +0530

    Logical (AND/OR) operators, WHILE and FOR loops.

--- lahari-jlox/src/jloxinterpreter/AstPrinter.java
+++ lahari-jlox/src/jloxinterpreter/AstPrinter.java
@@ -1,5 +1,7 @@
 package jloxinterpreter;
 
+import jloxinterpreter.Expr.Logical;
+
 class AstPrinter implements Expr.Visitor<String> {
     String print(Expr expr) {
         return expr.accept(this);
@@ -51,5 +53,11 @@ class AstPrinter implements Expr.Visitor<String> {
     public static void main(String[] args) {
         Expr expression = new Expr.Binary(new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(123)), new Token(TokenType.STAR, "*", null, 1), new Expr.Grouping(new Expr.Literal(45.67)));
         System.out.println(new AstPrinter().print(expression));
+    }
+
+    @Override
+    public String visitLogicalExpr(Logical expr) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitLogicalExpr'");
     }
 }
--- lahari-jlox/src/jloxinterpreter/PrintExpr.java
+++ lahari-jlox/src/jloxinterpreter/PrintExpr.java
@@ -1,5 +1,7 @@
 package jloxinterpreter;
 
+import jloxinterpreter.Expr.Logical;
+
 class PrintExpr implements Expr.Visitor<String> {
     String print(Expr expr) {
         return expr.accept(this);
@@ -52,5 +54,11 @@ class PrintExpr implements Expr.Visitor<String> {
     public static void main(String[] args) {
         Expr expression = new Expr.Binary(new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(123)), new Token(TokenType.STAR, "*", null, 1), new Expr.Grouping(new Expr.Literal(45.67)));
         System.out.println(new PrintExpr().print(expression));
+    }
+
+    @Override
+    public String visitLogicalExpr(Logical expr) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'visitLogicalExpr'");
     }
 }
